13eade84850cdc93457f7ad6ebce90fd554c5a99
deephead_peachapol <peachapol@deephead.com>
2023-07-06 23:38:19 +0700

    Fix Counter

---
+++ helpers/currency.ts
@@ -0,0 +1,5 @@
+
+
+export const getCurrencySymbol = (locale, currency) => {
+    return (0).toLocaleString(locale, { style: 'currency', currency, minimumFractionDigits: 0, maximumFractionDigits: 0 }).replace(/\d/g, '').trim()
+}
--- pages/transfer.tsx
+++ pages/transfer.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FunctionComponent, useEffect, useState } from "react";
+import { ChangeEvent, FunctionComponent, useEffect, useRef, useState } from "react";
 import MainLayout from "../layouts/MainLayout";
 import { Input, Space, Select, Button, InputNumber } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -12,6 +12,7 @@ import SelectCryptoModal from "../components/SelectCryptoModal";
 import useTokenList, { TokenListItem } from "../hooks/useTokenList";
 import useCurrencyList, { CurrencyListItem } from "../hooks/useCurrencyList";
 import SelectFiatModal from "../components/SelectFiatModal";
+import { getCurrencySymbol } from "../helpers/currency";
 
 export interface FeeBase {
   description: string;
@@ -41,9 +42,11 @@ const Trasfer: FunctionComponent = () => {
   const [selectedCrypto, setSelectedCrypto] = useState<TokenListItem>();
   const [selectedFiat, setSelectedFiat] = useState<CurrencyListItem>();
   const [quote, setQuote] = useState<QuoteData>();
+  const [currencySymbol, setCurrencySymbol] = useState("");
   const [cryptoAmount, setCryptoAmount] = useState(0);
   const [fiatAmount, setFiatAmount] = useState(0);
-  const [countDown, setCountDown] = useState(refreshRate);
+  const [counter, setCounter] = useState(refreshRate);
+  const counterRef = useRef<number>();
   const [chevron, setChevron] = useState(faChevronUp);
   const {tokenList} = useTokenList();
   const {currencyList} = useCurrencyList();
@@ -53,7 +56,7 @@ const Trasfer: FunctionComponent = () => {
       const res = await fetch("/api/offramp/quote?fiatCurrency={selectedFiat.id.toUpperCase()}&cryptoCurrency={selectedCrypto.symbol}&cryptoAmount={cryptoAmount}");
       const data = await res.json();
       setQuote({...data});
-      setFiatAmount(quote.fiatAmount);
+      setFiatAmount(data.fiatAmount);
     } catch (err) {
       console.log(err);
     }
@@ -61,23 +64,29 @@ const Trasfer: FunctionComponent = () => {
 
   useEffect(() => {
     callAPI();
-  }, [selectedFiat, selectedCrypto]);
+    if(selectedFiat != undefined){
+      console.log(selectedFiat.id.toUpperCase());
+      setCurrencySymbol(getCurrencySymbol('en-US', selectedFiat.id.toUpperCase()));
+    }
+    // console.log(getCurrencySymbol('en-US', 'CNY'));
+    // setCurrencySymbol(getCurrencySymbol('en-US', 'CNY'));
+  }, [cryptoAmount, selectedFiat, selectedCrypto]);
 
+  const decreaseNum = () => {
+    //console.log(num);
+    if(counterRef.current <= 0){
+      callAPI();
+      setCounter((prev) => refreshRate);
+    }
+    else{
+      setCounter((prev) => prev - 1);
+    }
+  }
+  useEffect(()=> {counterRef.current = counter}, [counter]);
   useEffect(() => {
-    const timer = setTimeout(function () {
-      if (countDown > 0) {
-        setCountDown(countDown - 1);
-      } else {
-        callAPI();
-        setCountDown(refreshRate);
-      }
-    }, 1000);
-
-    return () => {
-      // this should work flawlessly besides some milliseconds lost here and there
-      clearTimeout(timer);
-    };
-  }, [countDown]);
+    const timer= setInterval(decreaseNum, 1000);
+    return () => clearInterval(timer);
+  }, []);
 
   const toggleSummary = function (e) {
     if (chevron == faChevronUp) {
@@ -165,7 +174,7 @@ const Trasfer: FunctionComponent = () => {
         <FontAwesomeIcon className="text-[#eb98fd]" icon={faClock} />
         <span className="gradient-text">
           {" "}
-          Quote refresh in {countDown} secs
+          Quote refresh in {counter} secs
         </span>
       </div>
       <div className="bg-gray-200 opacity-90 p-3 align mx-3 my-6 align-middle rounded-xl text-xs text-default">
@@ -181,7 +190,7 @@ const Trasfer: FunctionComponent = () => {
         </div>
         { quote &&
           <div className="flex justify-between">
-            <div className="">{cryptoAmount} {selectedCrypto.symbol.toUpperCase()} @ {quote.conversionRate}</div>
+            <div className="">{cryptoAmount} {selectedCrypto.symbol.toUpperCase()} @ {currencySymbol}{quote.conversionRate}</div>
             <div>{fiatAmount}</div>
           </div>
         }
