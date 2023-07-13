10fb0c5128937a3ddaa5f4b93d87e9b10ca8ea06
deephead_peachapol <peachapol@deephead.com>
2023-07-14 00:04:59 +0700

    Update Footer Logo

--- components/UI/Footer.tsx
+++ components/UI/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className='absolute w-full fixed bottom-0 bg-white h-9 -z--1 rounded-b-3xl'>
       <div className='flex justify-center items-center h-full'>
-        <img src="/images/footer-logo.png" alt="" className='w-auto h-3' />
+        <img src="/images/footer-logo.svg" alt="" className='w-auto h-3' />
       </div>
     </footer>
   )
